ff8352b9de4fce669cd9df92d077999cb540576e
ankitbellurbis <153586848+ankitbellurbis@users.noreply.github.com>
2026-02-19 12:31:17 +0530

    Add tech animation page and sidebar navigation link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { FooterComponent } from './component/footer/footer.component';
 import { TrendingCarouselComponent } from './component/trending-carousel/trending-carousel.component';
 import { CompactMovieCardComponent } from './component/compact-movie-card/compact-movie-card.component';
 import { SafeUrlPipe } from './pipes/safe-url.pipe';
+import { TechAnimationComponent } from './pages/tech-animation/tech-animation.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { SafeUrlPipe } from './pipes/safe-url.pipe';
     FooterComponent,
     TrendingCarouselComponent,
     CompactMovieCardComponent,
-    SafeUrlPipe
+    SafeUrlPipe,
+    TechAnimationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/tech-animation/tech-animation.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tech-animation',
+  templateUrl: './tech-animation.component.html',
+  styleUrls: ['./tech-animation.component.scss']
+})
+export class TechAnimationComponent {}
