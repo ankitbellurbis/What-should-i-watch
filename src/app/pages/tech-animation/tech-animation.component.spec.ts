ede41f5912918518cce1b8ede49c7abc9d69e498
ankitbellurbis <153586848+ankitbellurbis@users.noreply.github.com>
2026-02-19 12:58:01 +0530

    Recreate tech animation component with iron arc reactor design

---
+++ src/app/pages/tech-animation/tech-animation.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TechAnimationComponent } from './tech-animation.component';
+
+describe('TechAnimationComponent', () => {
+  let component: TechAnimationComponent;
+  let fixture: ComponentFixture<TechAnimationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TechAnimationComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TechAnimationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
